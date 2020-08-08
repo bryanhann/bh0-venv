0dd3767e9739bd5b46afad19b1827d2749ba732f
No Name <noname@null.com>
2020-08-08 08:34:52 +0000

    Add installer.

--- activate.sh
+++ activate.sh
@@ -1,4 +1,12 @@
 pushd $(dirname $0)
 VENV_MAGIC786478=$PWD/build.venv
 popd
+venv_remove () { rm -rf $(venv_venv) ; }
 venv_venv () { echo $VENV_MAGIC786478 ; }
+venv_activate () { source $(venv_venv)/bin/activate ; }
+venv_install () {
+	python3 -m venv $(venv_venv)
+	venv_activate
+	python3 -m pip install --upgrade pip
+	deactivate
+}
