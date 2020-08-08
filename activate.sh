5a8ca558276ff95b2ab4b274800cfe2c22e3eee3
No Name <noname@null.com>
2020-08-08 11:58:05 +0000

    Include virtualenv and virtualenvwrapper.

--- activate.sh
+++ activate.sh
@@ -5,8 +5,14 @@ venv_remove () { rm -rf $(venv_venv) ; }
 venv_venv () { echo $VENV_MAGIC786478 ; }
 venv_activate () { source $(venv_venv)/bin/activate ; }
 venv_install () {
+	[[ -d $(venv_venv) ]] && {
+		bh0note venv already installed
+		return
+	}
 	python3 -m venv $(venv_venv)
 	venv_activate
 	python3 -m pip install --upgrade pip
+	python3 -m pip install virtualenv
+	python3 -m pip install virtualenvwrapper
 	deactivate
 }
