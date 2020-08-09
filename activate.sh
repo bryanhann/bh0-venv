d73b6bcd654539a35b7fd8d56c88fd27ba5fb0f9
Bryan Hann <nobody@nowhere>
2020-08-09 15:59:40 +0800

    Configure for bh0.

--- activate.sh
+++ activate.sh
@@ -1,18 +1,6 @@
-pushd $(dirname $0)
-VENV_MAGIC786478=$PWD/build.venv
-popd
-venv_remove () { rm -rf $(venv_venv) ; }
-venv_venv () { echo $VENV_MAGIC786478 ; }
-venv_activate () { source $(venv_venv)/bin/activate ; }
-venv_install () {
-	[[ -d $(venv_venv) ]] && {
-		bh0note venv already installed
-		return
-	}
-	python3 -m venv $(venv_venv)
-	venv_activate
-	python3 -m pip install --upgrade pip
-	python3 -m pip install virtualenv
-	python3 -m pip install virtualenvwrapper
-	deactivate
-}
+bh0include                              bh0.vwrap.VIRTUALENVWRAPPER_HOOK_DIR
+bh0export VIRTUALENVWRAPPER_HOOK_DIR  ~/bh0.vwrap.VIRTUALENVWRAPPER_HOOK_DIR
+bh0export WORKON_HOME                 $__LOCAL__/bh0.vwrap.WORKON_HOME
+bh0export PROJECT_HOME                $__LOCAL__/bh0.vwrap.PROJECT_HOME
+bh0export VIRTUALENVWRAPPER_PYTHON    $(which python3)
+bh0exec mkdir -p $PROJECT_HOME # vwrap does not create it automatically
