18cf21da20dac25231e76f65400326a63cf4c219
Bryan Hann <nobody@nowhere>
2020-08-10 12:54:17 +0800

    Use [$BH0__LOCAL__]

--- activate.sh
+++ activate.sh
@@ -1,6 +1,6 @@
 bh0include                              bh0.vwrap.hooks
 bh0export VIRTUALENVWRAPPER_HOOK_DIR  ~/bh0.vwrap.hooks
-bh0export WORKON_HOME                 $__LOCAL__/bh0.vwrap.WORKON_HOME
-bh0export PROJECT_HOME                $__LOCAL__/bh0.vwrap.PROJECT_HOME
+bh0export WORKON_HOME                 ${BH0__LOCAL__}/bh0.vwrap.WORKON_HOME
+bh0export PROJECT_HOME                ${BH0__LOCAL__}/bh0.vwrap.PROJECT_HOME
 bh0export VIRTUALENVWRAPPER_PYTHON    $(which python3)
 bh0exec mkdir -p $PROJECT_HOME # vwrap does not create it automatically
